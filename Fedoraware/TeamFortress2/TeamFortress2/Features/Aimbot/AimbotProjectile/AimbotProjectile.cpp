742c17b7e408ae60a25807625fb3cb82924b8806
johnpablo1 <roblexorg3@gmail.com>
2023-05-21 20:52:20 +0800

    Revert "More Shortening"
    This reverts commit 1b6ecfd248164da89301cf69c2e3db79b034609c.

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Aimbot/AimbotProjectile/AimbotProjectile.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Aimbot/AimbotProjectile/AimbotProjectile.cpp
@@ -1019,7 +1019,7 @@ bool CAimbotProjectile::GetSplashTarget(CBaseEntity* pLocal, CBaseCombatWeapon* 
 
 	std::optional<float> splashRadius;
 
-	splashRadius = Utils::ATTRIB_HOOK_FLOAT(148, "mult_explosion_radius", pWeapon);
+	splashRadius = Utils::ATTRIB_HOOK_FLOAT(148, "mult_explosion_radius", pWeapon, 0, 1);
 	float splashRadiusModified = splashRadius.value() * 0.8; //this value will only be used if you are blast jumping with the air strike
 
 	//check if you are rocket jumping, and change the value appropriately, because the air strike blast radius changes if you are rocket jumping.
